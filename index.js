207ed2eb57e1759ad4d5e62ffb64761e667079de
Max Milton <max@wearegenki.com>
2017-12-14 19:00:12 +1100

    POC of functionality

--- index.js
+++ index.js
@@ -21,4 +21,136 @@
  * limitations under the License.
  */
 
-// TODO
+const nodePath = require('path');
+const postcss = require('postcss');
+const postcssLoadConfig = require('postcss-load-config');
+const extend = require('raptor-util/extend');
+
+function configLoader(file) {
+  return (plugins, options) => {
+    if (Array.isArray(plugins)) {
+      return Promise.resolve({ plugins, options });
+    } else if (typeof plugins === 'function') {
+      return Promise.resolve(plugins(file));
+    }
+
+    const contextOptions = plugins || {};
+    let configPath;
+
+    if (contextOptions.config) {
+      if (nodePath.isAbsolute(contextOptions.config)) {
+        configPath = contextOptions.config;
+      } else {
+        configPath = nodePath.join(file.base, contextOptions.config);
+      }
+    } else {
+      configPath = file.dirname;
+    }
+    return postcssLoadConfig({ file, options: contextOptions }, configPath);
+  };
+}
+
+function getConfig(config, file) {
+  const loader = configLoader(file);
+
+  return loader().then((resConfig) => {
+    // console.log('@@@ RES CONFIG', resConfig);
+
+    const newConfig = Object.assign({}, resConfig);
+    const configOpts = config.options || {};
+    const resConfigOpts = resConfig.options || {};
+
+    newConfig.plugins = config.plugins || resConfig.plugins || {};
+    newConfig.options = Object.assign(resConfigOpts, configOpts);
+
+    return newConfig;
+  });
+}
+
+module.exports = function lassoPostcss(lasso, config) {
+  const postcssHandler = {
+    properties: {
+      path: 'string',
+      paths: 'string',
+      virtualPath: 'string',
+      code: 'string',
+      external: 'boolean',
+    },
+
+    init(lassoContext, cb) {
+      const { path } = this;
+
+      if (path || this.code) {
+        if (path) {
+          this.path = this.resolvePath(path);
+        }
+
+        this.postcssConfig = getConfig(config, this.path);
+      } else {
+        const pathError = new Error('"path" or "code" is required');
+        if (cb !== undefined) return cb(pathError);
+        throw pathError;
+      }
+
+      if (cb !== undefined) cb();
+    },
+
+    read(lassoContext, cb) {
+      return new Promise((resolve, reject) => {
+        if (!cb) {
+          cb = (err, res) => err ? reject(err) : resolve(res); // eslint-disable-line
+        }
+
+        const { path } = this;
+        const renderOptions = extend({}, config);
+
+        if (this.code) {
+          renderOptions.data = this.code;
+        } else if (path) {
+          renderOptions.file = path;
+        } else {
+          return cb(new Error('Invalid sass dependency. No path or code'));
+        }
+
+        this.postcssConfig.then((postcssConfig) => {
+          postcss(postcssConfig.plugins)
+            .process(renderOptions.data || renderOptions.file, postcssConfig.options)
+            .then((result) => {
+              console.log('@@@ result', result.css);
+
+              result.warnings().forEach((warn) => {
+                process.stderr.write(warn.toString());
+              });
+
+              cb(null, result.css);
+            })
+            .catch(cb);
+        });
+      });
+    },
+
+    getDir() {
+      if (this.dir) {
+        return this.dir;
+      }
+
+      const path = this.path || this.virtualPath;
+      return path ? nodePath.dirname(path) : undefined;
+    },
+
+    getLastModified(lassoContext, cb) {
+      return cb ? cb(null, -1) : -1;
+    },
+
+    calculateKey() {
+      return `less: ${(this.code || this.virtualPath || this.path)}`;
+    },
+  };
+
+  const extensions = config.extensions || ['css', 'pcss', 'postcss'];
+
+  extensions.forEach((ext) => {
+    const type = ext.charAt(0) === '.' ? ext.substring(1) : ext;
+    lasso.dependencies.registerStyleSheetType(type, postcssHandler);
+  });
+};
